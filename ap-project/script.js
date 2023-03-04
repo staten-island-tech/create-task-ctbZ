8e0d07bfb3b305c9e4fc49b6a36b769b7fc5177c
ctbZ <113132299+ctbZ@users.noreply.github.com>
2023-03-04 17:11:43 -0500

    added const
    added const and win condition array
    next, make function that starts the game
    
    -player X starts so player o turn set to false
    -add event listener click and hover effect makes X/O appear in the box

--- ap-project/script.js
+++ ap-project/script.js
@@ -1,9 +1,17 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
+const boxEl = document.querySelectorAll('child') //searches all elements in doc with child (includes?)
+const containerEl = document.getElementById('container-box')
+const winEl = document.getElementById('win')
+const winmsgEl = document.getElementById('winmsg')
+const restartbtn = document.getElementById('restartbtn')
+const player_x = 'x'
+const player_o = 'o'
+const win = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9],
+  [1,4,7],
+  [2,5,8],
+  [3,6,9],
+  [1,5,9],
+  [3,5,7]
+]
