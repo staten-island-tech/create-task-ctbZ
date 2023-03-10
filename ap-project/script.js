9ac6cb5e9c980d82c95da5a0d5bdc342809808aa
ctbZ <113132299+ctbZ@users.noreply.github.com>
2023-03-09 21:11:34 -0500

    Update script.js

--- ap-project/script.js
+++ ap-project/script.js
@@ -5,6 +5,7 @@ const winmsgEl = document.getElementById('winmsg')
 const restartbtn = document.getElementById('restartbtn')
 const player_x = 'x'
 const player_o = 'o'
+let player_o_turn = false //start with player x
 const win = [
   [1,2,3],
   [4,5,6],
@@ -15,3 +16,68 @@ const win = [
   [1,5,9],
   [3,5,7]
 ]
+
+start()
+
+restartbtn.addEventListener('click', start)
+
+function start() {
+  player_o_turn = false
+  boxEl.forEach(box => {
+    box.classList.remove(player_x)
+    box.classList.remove(player_o)
+    box.removeEventListener('click', boxclick)
+    box.addEventListener('click', boxclick, { once: true}) //prevent click agian on same box 
+  })
+  hover()
+  winmsgEl.classList.remove('show')
+}
+
+
+
+function boxclick(e) {
+  const box = e.target  //hover effect target box
+  const turn = player_o_turn ? player_o : player_x
+  mark( box, turn)
+  if (checkWin(turn)) {
+    gameover(false)
+  } else if (draw()) {
+    gameover(true)
+  } else {
+    swap()
+    hover()
+  }
+}
+
+function gameover(draw) {
+  if (draw) {
+    winmsgEl.innerText = "Draw Game"
+  } else {
+    winmsgEl.innerText = 'player ${player_o_turn ? "o" : "x"} wins'
+  }
+  winmsgEl.classList.add('show')
+}
+
+function draw() {
+	return [...boxEl].every(box => { //... spread syntax
+		return cell.classList.contains(player_x) || cell.classList.contains(player_o) //logical OR
+	})
+}
+
+function mark(box, turn) {
+  box.classList.add(turn)
+}
+
+function swap() {
+  player_o = !player_o_turn
+}
+
+function hover() {
+  containerEl.classList.remove(player_x)
+  containerEl.classList.remove(player_o)
+  if (player_o_turn) {
+    containerEl.classList.add(player_o)
+  } else {
+    containerEl.classList.add(player_x)
+  }
+}
