911947fa823b26991a82ece77d440f1acb4fcc45
ctbZ <113132299+ctbZ@users.noreply.github.com>
2023-03-14 20:10:17 -0400

    almost done
    -add restart btn
    -add draw game
    -glow css tic tac glow

--- ap-project/script.js
+++ ap-project/script.js
@@ -1,14 +1,58 @@
-const gameboard = document.querySelector("#board")
-const msg = document.querySelector('#info')
-const makeboxes = [
+const gameBoard = document.querySelector("#gameboard")
+const message = document.querySelector("#msg")
+const startGame = [
   "", "", "", "", "", "", "", "", ""
 ]
+let turn = "X"
+message.textContent = "Player X goes first"
 
-function creategameboard() {
-  makeboxes.forEach((box, index) => {
-    const box = document.createElement('div')
-    box.classList.add('box')
-    box.id = index
-    gameboard.append(box)
+function generateBoard() {
+  startGame.forEach((box, index) => {
+    const boxEl = document.createElement('div')
+    boxEl.classList.add('box')
+    boxEl.id = index
+    boxEl.addEventListener('click', placeMark)
+    gameBoard.append(boxEl)
   })
 }
+generateBoard()
+
+function placeMark(e) {
+  const mark = document.createElement('div')
+  mark.classList.add(turn)
+  e.target.append(mark)
+  turn = turn === "X" ? "O" : "X"
+  message.textContent = "It is player " + turn + "'s turn"
+  e.target.removeEventListener("click", placeMark)
+  checkWin()
+}
+
+function checkWin() {
+  const allBoxes = document.querySelectorAll(".box")
+  const win = [
+    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+  ]
+  win.forEach(array => {
+    const xWin = array.every(box => allBoxes[box].firstChild?.classList.contains('X'))
+
+    if(xWin) {
+     message.textContent = "Player X wins!"
+    allBoxes.forEach(box => box.replaceWith(box.cloneNode(true))) //cloneNode is to remove addEventListener
+    return
+   }
+  })
+
+  win.forEach(array => {
+    const oWin = array.every(box => allBoxes[box].firstChild?.classList.contains('O'))
+
+    if(oWin) {
+     message.textContent = "Player O wins!"
+    allBoxes.forEach(box => box.replaceWith(box.cloneNode(true))) //cloneNode is to remove addEventListener
+    return
+   } 
+  })
+
+  // if (!allSquares.includes(''))
+  // message.textContent = "Game Draw!"
+}
+
